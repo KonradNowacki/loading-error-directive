7a0092e4981ad9e53fbc0b025d684925f4eb9bd3
KonradNowacki <konrad.nowacki.sw@gmail.om>
2022-09-03 22:03:19 +0200

    Make it good workingg error wrapper + external loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,13 @@ import {delay, mapTo, Observable, of, tap} from "rxjs";
 export class AppComponent {
   title = 'loading-error-directive';
 
+  // Example observable returning correctly
   data1$: Observable<number> = of(null).pipe(
     delay(2000),
     mapTo(999)
   )
 
+  // Example observable throwing an Error
   data2$: Observable<string> = of(null).pipe(
     delay(4000),
     tap(() => {
@@ -23,6 +25,7 @@ export class AppComponent {
     mapTo('data 2 from observable')
   )
 
+  // Example observable returning correctly
   data3$: Observable<boolean> = of(null).pipe(
     delay(6000),
     mapTo(true)
